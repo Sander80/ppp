5491c7fffa8b32addfc4a011d75b69321ec2ceff
Jan Just Keijser <jan.just.keijser@gmail.com>
2020-05-28 11:24:13 +0200

    Fix gcc9+ compiler warning
    Signed-off-by: Jan Just Keijser <janjust@nikhef.nl>

--- pppd/eap-tls.c
+++ pppd/eap-tls.c
@@ -740,7 +740,7 @@ int eaptls_init_ssl_server(eap_state * esp)
 		return 0;
 	}
 
-	strncpy(ets->peer, esp->es_server.ea_peer, MAXWORDLEN);
+	strncpy(ets->peer, esp->es_server.ea_peer, MAXWORDLEN-1);
 
 	dbglog( "getting eaptls secret" );
 	if (!get_eaptls_secret(esp->es_unit, esp->es_server.ea_peer,
@@ -795,7 +795,7 @@ int eaptls_init_ssl_server(eap_state * esp)
 	 * so we can check it later in ssl_verify_callback()
 	 */
 	if (clicertfile[0])
-		strncpy(&ets->peercertfile[0], clicertfile, MAXWORDLEN);
+		strncpy(&ets->peercertfile[0], clicertfile, MAXWORDLEN-1);
 	else
 		ets->peercertfile[0] = 0;
 
