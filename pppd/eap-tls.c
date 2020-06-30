481b60df8040432d9ff5981d62c2e80dca8c564b
Jan Just Keijser <janjust@nikhef.nl>
2020-06-30 17:09:01 +0200

    Revert "Fix gcc9+ compiler warning"
    This reverts commit 30f398822df57362767c08df753ae1208928af3f.
    
    Signed-off-by: Jan Just Keijser <janjust@nikhef.nl>

--- pppd/eap-tls.c
+++ pppd/eap-tls.c
@@ -795,7 +795,7 @@ int eaptls_init_ssl_server(eap_state * esp)
 	 * so we can check it later in ssl_verify_callback()
 	 */
 	if (clicertfile[0])
-		strncpy(&ets->peercertfile[0], clicertfile, MAXWORDLEN-1);
+		strncpy(&ets->peercertfile[0], clicertfile, MAXWORDLEN);
 	else
 		ets->peercertfile[0] = 0;
 
